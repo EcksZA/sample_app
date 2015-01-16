1374e2388a3d3fbb51965ec9043e4d6f28984fb3
Xolani Mfuko <xmfuko@gmail.com>
2015-01-16 18:34:45 +0200

    edit file to include users#new

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get '/signup' => 'users#new'
 
   root to: 'static_pages#home'
-  
+
   get '/help' => 'static_pages#help'
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
