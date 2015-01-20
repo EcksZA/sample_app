bc8db9efb0829f8376ee94c0b763d1263a267bfd
Xolani Mfuko <xmfuko@gmail.com>
2015-01-20 18:59:21 +0200

    add password and password confirmation to strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
