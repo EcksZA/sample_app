73303120d00472027c44fc9bb4664dafc0720862
Xolani Mfuko <xmfuko@gmail.com>
2015-01-26 18:53:39 +0200

    pass integration tests for sign in

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe "Authentication" do
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_selector('h1', text: 'Sign in') }
+    it { should have_title('Sign in') }
+  end
+end
