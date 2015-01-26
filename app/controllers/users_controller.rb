2ebeeb360c1ba7030dc9369401c3b62ba1b34e8d
Xolani Mfuko <xmfuko@gmail.com>
2015-01-26 17:37:08 +0200

    add the create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render "new"
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
