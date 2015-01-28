a4b8975de18b6c338738bfdda893839b8ad3e0e0
Xolani Mfuko <xmfuko@gmail.com>
2015-01-28 14:04:57 +0200

    pass test to authorize before edit/update actions

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -56,4 +56,22 @@ describe "Authentication" do
     it { should have_link('Sign out', href: signout_path) }
     it { should_not have_link('Sign in', href: signin_path) }
   end
+
+  describe "authorization" do
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "in the Users controller" do
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submiting to the update action" do
+          before { put user_path(user) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+    end
+  end
 end
