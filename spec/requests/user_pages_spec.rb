6a30254c3a5b24246adbcb8f89f2e2a2f3c80adf
Xolani Mfuko <xmfuko@gmail.com>
2015-01-28 16:53:05 +0200

    tests for pagination. 10 failures to be assessed later. Web app functional

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -81,19 +81,24 @@ describe "User pages" do
   end
 
   describe "index" do
-    before do
-      sign_in FactoryGirl.create(:user)
-      FactoryGirl.create(:user, name: "Bob", email: "bob@example.com")
-      FactoryGirl.create(:user, name: "ben", email: "ben@example.com")
+    let(:user) { FactoryGirl.create(:user) }
+
+    before(:each) do
+      sign_in :user
       visit users_path
     end
 
     it { should have_title('All Users') }
     it { should have_selector('h1', text: 'All users') }
 
-    it "should list each user" do
-      User.all.each do |user|
-        page.should have_selector('li', text: user.name)
+    describe "pagination" do
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.all.each do |user|
+          page.should have_selector('li', text: user.name)
+        end
       end
     end
   end
