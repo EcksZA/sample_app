c29874a00ab4c8a474d3a4f9eadea5181e47c3c5
Xolani Mfuko <xmfuko@gmail.com>
2015-01-28 17:52:21 +0200

    pass test for admin capabilities

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,10 +13,13 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+
   it { should respond_to(:authenticate) }
   it { should respond_to(:remember_token) }
+  it { should respond_to(:admin) }
 
   it { should be_valid }
+  it { should_not be_admin }
 
   describe "when name is not present" do
     before { @user.name = "" }
@@ -102,5 +105,11 @@ describe User do
   describe "remember token" do
     before { @user.save }
     it { @user.remember_token.should_not be_blank }
+  end
+
+  describe "with admin attribute set to 'true'" do
+    before { @user.toggle!(:admin) }
+
+    it { should be_admin }
   end
 end
